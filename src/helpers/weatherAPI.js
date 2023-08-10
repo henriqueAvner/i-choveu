0ee046f15958fa64f7b2b030975f733cbe5045b3
henriqueAvner <avnerhdpb@gmail.com>
2023-08-10 19:47:20 -0300

    Requisito 1: Implementação da função searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,17 @@
 // Remova os comentários a medida que for implementando as funções
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  try {
+    const API_CITY = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=adc8c4af72734b528fd220507231008&q=${term}`);
+    const data = await API_CITY.json();
+    if (data.length > 0) {
+      return data;
+    }
+    window.alert('Nenhuma cidade encontrada');
+    return [];
+  } catch (err) {
+    alert(err.message);
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
